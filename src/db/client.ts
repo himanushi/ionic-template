0e216eb62b0a3d67c4ba7c92bfd8fa68ebaa91ad
himanushi <himanushi@users.noreply.github.com>
2023-11-21 20:22:37 +0900

    add client

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,10 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: "host",
-  user: "user",
-  database: "database",
-});
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+};
+
+export const connection = await mysql.createConnection(dbConfig);
 
 export const db = drizzle(connection);
