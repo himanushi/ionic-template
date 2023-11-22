4cc5156e042a2bceaf547c57c82e05048e632516
himanushi <himanushi@users.noreply.github.com>
2023-11-22 19:22:52 +0900

    add schema

---
+++ src/db/schema.ts
@@ -0,0 +1,12 @@
+import { bigint, index, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+
+export const users = mysqlTable(
+  "users",
+  {
+    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+    fullName: varchar("full_name", { length: 256 }),
+  },
+  (users) => ({
+    nameIdx: index("name_idx").on(users.fullName),
+  }),
+);
